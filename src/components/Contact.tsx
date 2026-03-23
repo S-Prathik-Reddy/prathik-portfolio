0cf297554f09924249a5d06f374d8b9025d3a6cb
S-Prathik-Reddy <86648896+S-Prathik-Reddy@users.noreply.github.com>
2026-03-23 10:08:31 -0500

    Create Contact.tsx

---
+++ src/components/Contact.tsx
@@ -0,0 +1,62 @@
+import { MdArrowOutward, MdCopyright } from "react-icons/md";
+import "./styles/Contact.css";
+
+const Contact = () => {
+  return (
+    <div className="contact-section section-container" id="contact">
+      <div className="contact-container">
+        <h3>Contact</h3>
+        <div className="contact-flex">
+          <div className="contact-box">
+            <h4>Email</h4>
+            <p>
+              <a href="mailto:sreddy.prathik@gmail.com" data-cursor="disable">
+                sreddy.prathik@gmail.com
+              </a>
+            </p>
+            <h4>Education</h4>
+            <p>MS in Computer Science, Syracuse University</p>
+          </div>
+          <div className="contact-box">
+            <h4>Social</h4>
+            <a
+              href="https://github.com/S-Prathik-Reddy"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Github <MdArrowOutward />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/prathik-sannapureddy/"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Linkedin <MdArrowOutward />
+            </a>
+            <a
+              href="https://www.instagram.com/_prathik_reddy/"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Instagram <MdArrowOutward />
+            </a>
+          </div>
+          <div className="contact-box">
+            <h2>
+              Designed and Developed <br /> by{" "}
+              <span>Prathik Reddy Sannapureddy</span>
+            </h2>
+            <h5>
+              <MdCopyright /> 2025
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
